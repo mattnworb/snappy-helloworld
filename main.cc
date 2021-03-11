ccef19298f03ce40eaf21098ec47c485a317e9c1
Matt Brown <mattbrown@spotify.com>
2021-03-11 18:12:24 +0000

    change program to read input_string from argv

--- main.cc
+++ main.cc
@@ -5,15 +5,25 @@
 
 using namespace std;
 
-int main() {
-  string input_string = "hello world! abc123 abc123abc123 abc123 abc123 abc123 abc123 abc123 abc123 abc123"s;
+int main(int argc, char** argv) {
+  string input_string = "";
 
-  cout << "input is " << input_string.length() << " in size:" << endl;
-  cout << input_string << endl;
+  if (argc == 1) {
+    cerr << "Usage: " << argv[0] << " <input string ...>" << endl;
+    return 1;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    input_string.append(argv[i]);
+    input_string.append(" ");
+  }
+
+  cout << "input has size=" << input_string.length() << ":" << endl;
+  cout << input_string << endl << endl;
 
   string compressed;
   size_t compressed_length = snappy::Compress(input_string.data(), input_string.length(), &compressed);
 
-  cout << "compressed output is " << compressed_length << " in size:" << endl;
+  cout << "compressed output has size=" << compressed_length << ":" << endl;
   cout << compressed << endl;
 }
